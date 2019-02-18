b7b4d6dddee23bf7822d8664bbd849bd5f50ba74
wdy <wdy0808@outlook.com>
2019-02-18 15:36:35 +0800

    finish f1 score

--- score.py
+++ score.py
@@ -1,7 +1,8 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import train_test_split
-from sklearn import metrics
+from sklearn.metrics import make_scorer
+from sklearn.metrics import f1_score
 
 label_file_name = "./data/cora/cora.content"
 embedding_file_name = "data/cora/output.embeddings"
@@ -33,6 +34,7 @@ with open(embedding_file_name) as f:
         index = index + 1
 
 percent = [0.9, 0.7, 0.5]
+scores = [[]]
 
 for train_perc in percent:
 
@@ -40,8 +42,13 @@ for train_perc in percent:
         datas, labels, test_size=train_perc, random_state=42)
 
     classifier = LogisticRegression(penalty='l2')
-    scores = cross_val_score(classifier, Data_train, label_train, cv=5)
+    scorer_micro = make_scorer(f1_score, average='micro')
+    scorer_macro = make_scorer(f1_score, average='macro')
+    scores_micro = cross_val_score(classifier, Data_train, label_train, cv=5, scoring=scorer_micro)
+    scores_macro = cross_val_score(classifier, Data_train, label_train, cv=5, scoring=scorer_macro)
     #classifier.fit(Data_train, label_train)
     #x = classifier.predict(Data_test) 
+    scores.append(scores_micro)
+    scores.append(scores_macro)
 
-    print(scores)
+print(scores)
